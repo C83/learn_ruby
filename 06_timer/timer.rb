8d7adc50c1d68aafb57f5f041942db530f75898e
Cyril <manda.cyril@gmail.com>
2018-04-09 22:56:46 +0200

    Meilleure utilisation fonction avec deux valeurs de retour

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -10,22 +10,19 @@ class Timer
 
   def time_string
   	# Prise en compte du format spécifique des timers
-  	result = format_temps @seconds 
-  	secondsAffichees = result[1]
+    minutesAffichees, secondsAffichees = format_temps @seconds 
+  	minutesAffichees += @minutes
+  	
+    heuresAffichees, minutesAffichees = format_temps minutesAffichees
+  	heuresAffichees += @heures
 
-  	minutesAffichees = @minutes + result[0]
-  	result = format_temps minutesAffichees
-
-  	minutesAffichees = result[1]
-  	heuresAffichees = @heures + result[0] 
-
-  	# On peut spécifier le format grâce aux "%format" %var 
-  	# %02d signifie que je veux deux chiffres pour indiquer la valeur de la variable
   	return padded(heuresAffichees) + ":" + padded(minutesAffichees) + ":" + padded(secondsAffichees)
   end
 
   # Fonction devrait être private mais elle est public pour être testée 
   def padded (x)
+    # On peut spécifier le format grâce aux "%format" %var 
+    # %02d signifie que je veux deux chiffres pour indiquer la valeur de la variable
     return "%02d" %x
   end
 
@@ -34,6 +31,6 @@ class Timer
   def format_temps (nb)
   	supp = nb/60
   	nbU = nb%60
-  	return supp, nbU # la fonction retourne un tableau 
+  	return supp, nbU # la fonction retourne deux valeurs 
   end
 end
